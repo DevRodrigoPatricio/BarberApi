d38f06f86e002b0cb9f018af1060de8880fcb69e
DevRodrigoPatricio <rodrigopatricio19@gmail.com>
2024-10-11 20:31:37 -0300

    Atualizações

--- BarberBackEnd/src/main/java/com/barber/Services/SchedulingService.java
+++ BarberBackEnd/src/main/java/com/barber/Services/SchedulingService.java
@@ -2,13 +2,13 @@ package com.barber.Services;
 
 import com.barber.Entities.Barber;
 import com.barber.Entities.Dtos.SchedulingDTO;
+import com.barber.Entities.Enums.Status;
 import com.barber.Entities.Scheduling;
 import com.barber.Entities.Services;
 import com.barber.Entities.User;
 import com.barber.Exceptions.ObjectnotFoundException;
 import com.barber.Repositories.BarberRepository;
 import com.barber.Repositories.SchedulingRepository;
-import com.barber.Repositories.ServiceRepository;
 import com.barber.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,7 +31,7 @@ public class SchedulingService {
     private UserRepository userRepository;
 
     @Autowired
-    private ServicesService  servicesService;
+    private ServicesService servicesService;
 
     public List<SchedulingDTO> findAll() {
         List<Scheduling> schedulings = repository.findAll();
@@ -43,24 +43,34 @@ public class SchedulingService {
         return scheduling.orElseThrow(() -> new ObjectnotFoundException("agendamento não localizado " + id));
     }
 
-    public SchedulingDTO create(SchedulingDTO schedulingDTO) {
+    public SchedulingDTO create( SchedulingDTO schedulingDTO) {
 
         User user = userRepository.findById(schedulingDTO.getUser())
                 .orElseThrow(() -> new ObjectnotFoundException("usuário não encontrado!"));
 
         Barber barber = barberRepository.findById(schedulingDTO.getBarber())
-                .orElseThrow(() -> new ObjectnotFoundException("Barberiro não encontrado!"));
+                .orElseThrow(() -> new ObjectnotFoundException("Barbeiro não encontrado!"));
 
         Set<Services> services = new HashSet<Services>();
 
-        for (Integer idService : schedulingDTO.getServices()){
-            Services service =  servicesService.findById(idService);
+        for (Integer idService : schedulingDTO.getServices()) {
+            Services service = servicesService.findById(idService);
             services.add(service);
         }
 
-        Scheduling scheduling = new Scheduling(schedulingDTO, user, barber,services);
+        Scheduling scheduling = new Scheduling(schedulingDTO, user, barber, services);
         repository.save(scheduling);
         return new SchedulingDTO(scheduling);
+    }
+
+    public SchedulingDTO update(int id, Status status) {
+
+        Scheduling OldScheduling = repository.findById(id).orElseThrow(() ->
+                new ObjectnotFoundException("Agendamento não localizado."));
+
+        OldScheduling.setStatus(status);
+        return new SchedulingDTO(repository.save(OldScheduling));
+
     }
 
 
